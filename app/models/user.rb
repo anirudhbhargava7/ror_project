3431a9d06f7e32acc9edeb504347f7ce8f8eb87d
Anirudh <anirudhbhargava7@gmail.com>
2013-09-07 08:58:45 -0700

    Add SSL in production

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,11 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :name, :password , :password_confirmation
-validates :email ,:presence=>true, :uniqueness=>true, :allow_blank => true
+validates :email ,:presence=>true, :uniqueness=>true
 
-validates :name ,:presence => true, :allow_blank => true
+validates :name ,:presence => true
 
-validates :password, :presence=>true, :allow_blank=>true, :confirmation =>true ,:length => {
-	:minimum => 6,
-	:allow_blank=>true 
+validates :password, :presence=>true,:confirmation =>true ,:length => {
+	:minimum => 6
 }
 
 
